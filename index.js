d431608504da2e95cf352b2285df8e7e4bf27670
Olivia Silva <olivia.silva@wework.com>
2019-01-27 00:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ musicianInstrument[i]=musicianString;
 return musicianInstrument;
 }
 
+const facts=[
+`He was the last Beatle to learn to drive`,
+`He was never a vegetarian`,
+`He was a choir boy and boy scout`,
+`He hated the sound of his own voice`
+];
+function johnLennonFacts(factsArray){
+var i=0;
+var newArray=[];
+while (factsArray.length > i){
+newArray.push(`${factsArray[i]}` + `!!!`);
+i++;
+}
+return newArray;
+}
